0758e57b3b211fe9696dd98a6eb4ead663ed55f8
Niklas Drews <niklas.drews@outlook.com>
2026-02-05 17:40:29 +0100

    - convert response time differences to list[float] instead of list[decimal] to fix 'TypeError: conversion from numpy.int64 to Decimal is not supported' in ResponseTime.getVar() - convert packet time differences to list[float] instead of list[decimal] to fix 'TypeError: conversion from numpy.int64 to Decimal is not supported' in PacketTime.getVar()

--- meter/features/packet_time.py
+++ meter/features/packet_time.py
@@ -23,7 +23,7 @@ class PacketTime:
         if self.packet_times is not None:
             return self.packet_times
         first_packet_time = self.flow.packets[0][0].time
-        packet_times = [packet.time - first_packet_time for packet, _ in self.flow.packets]
+        packet_times = packet_times = [float(packet.time - first_packet_time) for packet, _ in self.flow.packets]
         return packet_times
 
     def relative_time_list(self):
