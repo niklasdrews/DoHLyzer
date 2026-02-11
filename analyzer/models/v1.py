b132ce1033b196120297f51e0c9619b5068c27ed
Niklas Drews <niklas.drews@outlook.com>
2026-02-11 20:06:52 +0100

    -fix  tensorflow imports, package structure was changed

--- analyzer/models/v1.py
+++ analyzer/models/v1.py
@@ -4,7 +4,8 @@ from keras.src.layers import Dense, Dropout, LSTM
 
 def create_model(segment_size):
     model = Sequential()
-    model.add(LSTM(segment_size * 8, input_shape=(segment_size, 5), activation='relu'))
+    model.add(
+        LSTM(segment_size * 8, input_shape=(segment_size, 5), activation='relu'))
     model.add(Dense(segment_size * 6, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(segment_size * 2, activation='relu'))
--- analyzer/models/v3.py
+++ analyzer/models/v3.py
@@ -5,7 +5,8 @@ from keras.layers import Dense, Dropout, MaxPool1D, Conv1D
 
 def create_model(segment_size):
     model = Sequential()
-    model.add(Conv1D(segment_size * 2, kernel_size=3, input_shape=(segment_size, 5), activation='relu'))
+    model.add(Conv1D(segment_size * 2, kernel_size=3,
+              input_shape=(segment_size, 5), activation='relu'))
     model.add(MaxPool1D())
     model.add(Flatten())
     model.add(Dense(segment_size * 6, activation='relu'))
